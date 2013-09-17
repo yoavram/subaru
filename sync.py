b161d0ac8a3f1c69ab47efcc78d0c5fd20f7dbc8
Yoav Ram <yoavram@gmail.com>
2013-09-17 22:05:24 +0300

    based on ofer moshaiof's gist: https://gist.github.com/yoavram/07c3f0957dfa40afaf2e

---
+++ sync.py
@@ -0,0 +1,35 @@
+import sys
+import datetime 
+
+DATE_FORMAT = '%H:%M:%S,%f'
+
+def shift(time_str, seconds):
+  time = datetime.datetime.strptime(time_str, DATE_FORMAT);
+  time_plus_seconds = time + datetime.timedelta(seconds=seconds);
+  back_to_string = datetime.datetime.strftime(time_plus_seconds, DATE_FORMAT)[:-3];
+  return back_to_string;
+
+file_name = sys.argv[1]
+seconds = int(sys.argv[2])
+output_name = file_name[:-4] + '_shifted.srt'
+print "Shifting", file_name, "by", seconds, "seconds"
+print "Writing output to",output_name
+f = open(file_name, 'r')
+f_new = open(output_name, 'w')
+
+for line in f:
+  if '-->' in line:
+    #print(line);
+    from_string = line[:12]
+    from_string_plus_seconds = shift(from_string, seconds)
+    
+    to_string = line[17:-1]
+    to_string_plus_seconds = shift(to_string, seconds)
+  
+    f_new.write(from_string_plus_seconds + ' --> ' + to_string_plus_seconds + '\n')
+  else:
+    f_new.write(line);
+
+f.close()
+f_new.close()
+print "Finished"
